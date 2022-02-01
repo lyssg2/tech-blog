e73994f56448e58bbed19fda9d2fa76cc5c4f9b2
Lyss Garcia <alyssa.g200@gmail.com>
2022-01-31 19:13:06 -0800

    trying with heroku

--- controllers/dash-routes.js
+++ controllers/dash-routes.js
@@ -29,24 +29,24 @@ router.get('/new-post', withAuth, (req, res) => {
 })
 
 // When post is clicked, edit view is passed through template
-// router.get('/edit/:id', withAuth, async(req, res) => {
-//     try {
-//         const postData = await Post.findByPk(req.params.id);
+router.get('/edit/:id', withAuth, async(req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id);
 
-//         if (postData) {
-//             const post = postData.get({ plain: true });
+        if (postData) {
+            const post = postData.get({ plain: true });
 
-//             res.render('edit-post', {
-//                 layout: 'dashboard',
-//                 post
-//             });
-//         } else {
-//             res.status(404).end();
-//         };
-//     } catch (err) {
-//         res.redirect('login');
-//     }
-// })
+            res.render('edit-post', {
+                layout: 'dashboard',
+                post
+            });
+        } else {
+            res.status(404).end();
+        };
+    } catch (err) {
+        res.redirect('login');
+    }
+})
 
 
 module.exports = router;
